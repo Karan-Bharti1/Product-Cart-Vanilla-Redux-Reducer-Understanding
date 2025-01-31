f8c9104d794f97d038e7b465d49408b0209c7807
Karan-Bharti1 <karan.codes1@gmail.com>
2025-01-31 18:58:28 +0530

    chore: added data for products

--- src/main.js
+++ src/main.js
@@ -1,2 +1,17 @@
 import './style.css'
-
+const Products=[{
+  name:"Product A",
+  price:20,
+  quantity:1
+},
+  {
+    name:"Product B",
+    price:40,
+    quantity:1
+  },
+  {
+    name:"Product C",
+    price:50,  
+    quantity:1
+  }
+]
